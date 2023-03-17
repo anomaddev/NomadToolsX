83a066e70cc17a908a3f50e9e58b45284d91159e
Justin Ackermann <anomaddev@gmail.com>
2023-03-16 23:36:44 -0400

    working

---
+++ Sources/NomadToolsX/UI/Icon.swift
@@ -0,0 +1,58 @@
+//
+//  Icon.swift
+//  
+//
+//  Created by Justin Ackermann on 3/16/23.
+//
+
+import UIKit
+
+public protocol Asset {
+    var image: UIImage? { get }
+}
+
+public enum Icon: String, CaseIterable, Asset {
+    
+    case backArrow
+    case bell
+    
+    case calendar
+    
+    case flashOff
+    case flashOn
+    
+    case imprt
+    
+    case list
+    
+    case menu
+    case menu2
+    
+    case plus
+    
+    case qrcode
+    
+    case search
+    
+    case world
+    
+    public var image: UIImage? {
+        return UIImage(
+            named: rawValue,
+            in: .module,
+            compatibleWith: nil
+        )
+    }
+}
+
+extension UIImageView {
+    public func tinted(icon: Icon, _ color: UIColor) {
+        image = icon.image
+        tintColor = color
+    }
+    
+    public func tinted(asset: Asset, _ color: UIColor) {
+        image = asset.image
+        tintColor = color
+    }
+}
