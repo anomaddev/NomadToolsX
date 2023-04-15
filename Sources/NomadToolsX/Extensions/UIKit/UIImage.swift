3c304923f620805f8dbee2305d2fff29a576c357
Justin Ackermann <anomaddev@gmail.com>
2023-04-14 20:57:27 -0400

    working

--- Sources/NomadToolsX/Extensions/UIKit/UIImage.swift
+++ Sources/NomadToolsX/Extensions/UIKit/UIImage.swift
@@ -33,4 +33,73 @@ public extension UIImage {
     func rotate(degrees: Float) -> UIImage? {
         rotate(radians: degrees * .pi/180)
     }
+    
+    /// Crop image to remove transparent pixels surrounding image
+    func cropImageByAlpha() -> UIImage {
+        let cgImage = self.cgImage
+        let context = createARGBBitmapContextFromImage(inImage: cgImage!)
+        let height = cgImage!.height
+        let width = cgImage!.width
+        
+        var rect: CGRect = CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height))
+        context?.draw(cgImage!, in: rect)
+        
+        let pixelData = self.cgImage!.dataProvider!.data
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        
+        var minX = width
+        var minY = height
+        var maxX: Int = 0
+        var maxY: Int = 0
+        
+        //Filter through data and look for non-transparent pixels.
+        for y in 0..<height {
+            for x in 0..<width {
+                let pixelIndex = (width * y + x) * 4 /* 4 for A, R, G, B */
+                
+                if data[Int(pixelIndex)] != 0 { //Alpha value is not zero pixel is not transparent.
+                    if (x < minX) {
+                        minX = x
+                    }
+                    if (x > maxX) {
+                        maxX = x
+                    }
+                    if (y < minY) {
+                        minY = y
+                    }
+                    if (y > maxY) {
+                        maxY = y
+                    }
+                }
+            }
+        }
+        
+        rect = CGRect( x: CGFloat(minX), y: CGFloat(minY), width: CGFloat(maxX-minX), height: CGFloat(maxY-minY))
+        let imageScale:CGFloat = self.scale
+        let cgiImage = self.cgImage?.cropping(to: rect)
+        return UIImage(cgImage: cgiImage!, scale: imageScale, orientation: self.imageOrientation)
+    }
+    
+    private func createARGBBitmapContextFromImage(inImage: CGImage) -> CGContext? {
+        
+        let width = cgImage!.width
+        let height = cgImage!.height
+        
+        let bitmapBytesPerRow = width * 4
+        let bitmapByteCount = bitmapBytesPerRow * height
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        if colorSpace == nil {
+            return nil
+        }
+        
+        let bitmapData = malloc(bitmapByteCount)
+        if bitmapData == nil {
+            return nil
+        }
+        
+        let context = CGContext (data: bitmapData, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bitmapBytesPerRow, space: colorSpace, bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)
+        
+        return context
+    }
 }
