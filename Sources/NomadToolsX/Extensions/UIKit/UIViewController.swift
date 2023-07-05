805040e1796513863853403f12fa7aadd1bd55b7
Justin Ackermann <justinackermann@yahoo.com>
2023-07-05 00:24:56 -0400

    --quick work

--- Sources/NomadToolsX/Extensions/UIKit/UIViewController.swift
+++ Sources/NomadToolsX/Extensions/UIKit/UIViewController.swift
@@ -9,5 +9,21 @@ import UIKit
 
 extension UIViewController {
     
-    
+    /// This finds the current UIScreen with context instead of a shared instance
+    ///
+    /// - returns: `UIScreen`
+    ///
+    public func screen() throws -> UIScreen {
+        var parent = self.parent
+        var lastParent = parent
+        
+        while parent != nil {
+            lastParent = parent
+            parent = parent!.parent
+        }
+        
+        guard let screen = lastParent?.view.window?.windowScene?.screen
+        else { throw NSError() }
+        return screen
+    }
 }
