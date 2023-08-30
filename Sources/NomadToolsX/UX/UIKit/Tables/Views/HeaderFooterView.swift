280b06ee129761b334cfaa3624fc0c833b2402d3
Justin Ackermann <anomaddev@gmail.com>
2023-08-30 18:00:15 -0400

    --HeaderFooterView optimization in custom layout

--- Sources/NomadToolsX/UX/UIKit/Tables/Views/HeaderFooterView.swift
+++ Sources/NomadToolsX/UX/UIKit/Tables/Views/HeaderFooterView.swift
@@ -15,8 +15,14 @@ open class HeaderFooterView: UITableViewHeaderFooterView {
     public static func getId() -> String { id }
     open class var id: String { return "headerFooter" }
     
-    public lazy var text: Header = Header(height: 20)
-    public lazy var subtext: Header = Header(style: .H3, alignment: .right)
+    public lazy var row: Row = Row([
+        .distribution(.fill),
+        .alignment(.center),
+        .spacing(10)
+    ])
+    
+    public var titleView: Header!
+    public lazy var subtitleView: Header = Header(style: .H3, alignment: .right)
     
     public static func register(on table: UITableView)
     { table.register(self, forHeaderFooterViewReuseIdentifier: id)}
@@ -24,28 +30,16 @@ open class HeaderFooterView: UITableViewHeaderFooterView {
     public static func dequeue(on table: UITableView) -> HeaderFooterView?
     { return table.dequeueReusableHeaderFooterView(withIdentifier: id) as? HeaderFooterView }
     
-    open func layout(with model: HeaderFooterViewModel) {
-        
-    }
-    
-    open func layout(with text: String? = nil,
-                     sub subtext: String? = nil,
-                     pad padding: CGFloat! = 10,
+    open func layout(title: String? = nil,
+                     subtitle: String? = nil,
+                     insets: UIEdgeInsets? = .surrounding(vertical: 5, horizontal: 15),
                      style header: Header.HeaderStyle! = .H3) {
-        self.text = Header(style: header, height: 20)
-        self.text.text = text
-        self.subtext.text = subtext
-        add(self.text, self.subtext)
-        constrain(self.text, self.subtext)
-        { text, sub in
-            let superview = text.superview!
-            text.left ~== superview.left + 15
-            text.centerY ~== superview.centerY
-            text.top ~== superview.top + padding
-            
-            sub.right ~== superview.right - 15
-            sub.centerY ~== superview.centerY
-        }
+        titleView = Header(title, style: header, height: 20)
+        subtitleView.text = subtitle
+        
+        row.addArrangedSubview(titleView)
+        row.addArrangedSubview(subtitleView)
+        row.fitTo(self, padding: insets)
         
         layoutIfNeeded()
     }
